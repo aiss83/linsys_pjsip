ce1f607d291bda52d3a1059a6575ebb29785c3b6
Aleksandr Shaikhrazeev <alexander.shaykhrazeev@gmail.com>
2021-09-28 11:04:07 +0500

    Fixed makefiles and thread types to get compile

--- third_party/webrtc_aec3/src/rtc_base/platform_thread_types.h
+++ third_party/webrtc_aec3/src/rtc_base/platform_thread_types.h
@@ -39,7 +39,7 @@ typedef DWORD PlatformThreadRef;
 typedef zx_handle_t PlatformThreadId;
 typedef zx_handle_t PlatformThreadRef;
 #elif defined(WEBRTC_POSIX)
-typedef pid_t PlatformThreadId;
+typedef pthread_t PlatformThreadId;
 typedef pthread_t PlatformThreadRef;
 #endif
 
